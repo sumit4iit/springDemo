108323e3c8c78faa91c53017ee9200f12bff79b1
sumit agrawal <sumit4iit@gmail.com>
2016-03-15 00:39:05 +0530

    First Commit

---
+++ src/demo/spring/DrawingApp.java
@@ -0,0 +1,23 @@
+package demo.spring;
+
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.beans.factory.xml.XmlBeanFactory;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.core.io.FileSystemResource;
+
+public class DrawingApp {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		@SuppressWarnings("deprecation")
+//		BeanFactory factory = new XmlBeanFactory(new FileSystemResource("spring.xml"));
+		ApplicationContext context = new ClassPathXmlApplicationContext("spring.xml");
+		Triangle triangle = (Triangle) context.getBean("triangle");
+		triangle.draw();
+	}
+
+}
---
+++ src/demo/spring/Triangle.java
@@ -0,0 +1,65 @@
+package demo.spring;
+
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.BeanNameAware;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+
+public class Triangle implements ApplicationContextAware, BeanNameAware {
+	
+
+
+	public Point getPointA() {
+		return pointA;
+	}
+
+
+	public void setPointA(Point pointA) {
+		this.pointA = pointA;
+	}
+
+
+	public Point getPointB() {
+		return pointB;
+	}
+
+
+	public void setPointB(Point pointB) {
+		this.pointB = pointB;
+	}
+
+
+	public Point getPointC() {
+		return pointC;
+	}
+
+
+	public void setPointC(Point pointC) {
+		this.pointC = pointC;
+	}
+
+
+	private Point pointA;
+	private Point pointB;
+	private Point pointC;
+	private ApplicationContext context;
+
+	public void draw() {
+		System.out.println("Point B: " + pointB.getX() + ":" + pointC.getX() );
+	}
+
+
+	@Override
+	public void setApplicationContext(ApplicationContext context)
+			throws BeansException {
+		this.context = context;
+		
+	}
+
+
+	@Override
+	public void setBeanName(String name) {
+		System.out.println("bean name is :" + name);
+		
+	}
+}
